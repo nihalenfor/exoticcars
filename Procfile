5c83df8099e783289b3519b88e7211058cd01ab2
nihalenfor <kurumbathodinihal@gmail.com>
2022-03-03 14:53:44 +0530

    version2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn car_project.wsgi --log-file -
--- car_project/settings.py
+++ car_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3t6zjze=g@id&$(+r2dfu(kte=oplrb05ymy9@y6b9&q=$lit%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
